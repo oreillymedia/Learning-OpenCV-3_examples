cdf1b8052526788f3448e914bb612b994ec3d600
Gary Bradski <garybradskigit@gmail.com>
2017-05-24 16:43:29 -0700

    Update example_09-05.cpp
    added example

--- example_09-05.cpp
+++ example_09-05.cpp
@@ -1,3 +1,7 @@
+//Example 9-5. An example program ch4_qt.cpp, which takes a single argument
+//indicating a video file; that video file will be replayed inside of a Qt object that we will
+//define, called QMoviePlayer
+//
 #include <QApplication>
 #include <QLabel>
 #include <QMoviePlayer.hpp>
