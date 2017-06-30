a533357fa46489fa6d03442ee6e709fbfb7d68cb
Gary Bradski <garybradskigit@gmail.com>
2017-06-30 14:40:31 -0700

    better description file

--- example_09-05.cpp
+++ example_09-05.cpp
@@ -5,7 +5,19 @@
 #include <QApplication>
 #include <QLabel>
 #include <QMoviePlayer.hpp>
+#include <iostream>
+
+using namespace std;
+
 int main( int argc, char* argv[] ) {
+  cout << "\nExample 9-5. An example program ch4_qt.cpp, which takes a single argument"
+       << "\nindicating a video file; that video file will be replayed inside of a Qt object"
+       << "\nthat we will define, called QMoviePlayer"
+       << "\nCall:\n" << argv[0] << " <path/movie>"
+       << "\nExample:\n" << argv[0] << " ../tree.avi\n" << endl;
+  if(argc != 2)
+    return -1;
+  
   QApplication app( argc, argv );
   QMoviePlayer mp;
   mp.open( argv[1] );
